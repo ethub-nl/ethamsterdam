5e3d5e572370647258714c0b90cddf9c85a56f7d
wslyvh <wslyvh@users.noreply.github.com>
2019-10-18 14:49:01 +0200

    deploy

--- deploy.js
+++ deploy.js
@@ -8,19 +8,22 @@ var config = {
   host: process.env.FTP_HOST,
   port: 21,
   localRoot: __dirname + "/web/",
-  remoteRoot: "/public_html/",
+  remoteRoot: "/public_html/css",
   include: ["*", "**/*"],
   exclude: [],
   deleteRemote: false,
   forcePasv: true
 };
 
+// use with callback
 ftpDeploy.deploy(config, function(err, res) {
   if (err) {
+    console.log("Error deploying..");
     console.log(err);
+    process.exit(1);
   } else {
-    console.log("finished:", res);
+    console.log("Finished deployment..");
+    console.log(res);
+    process.exit(0);
   }
 });
-
-console.log("All done..");
