c5ac8314a4c14f72173c0dc922aaa133bff5b5cc
wslyvh <wslyvh@users.noreply.github.com>
2019-10-18 14:57:30 +0200

    deploy

--- deploy.js
+++ deploy.js
@@ -15,7 +15,21 @@ var config = {
   forcePasv: true
 };
 
-// use with callback
+ftpDeploy.on("uploading", function(data) {
+  console.log(data.totalFilesCount); // total file count being transferred
+  console.log(data.transferredFileCount); // number of files transferred
+  console.log(data.filename); // partial path with filename being uploaded
+});
+ftpDeploy.on("uploaded", function(data) {
+  console.log(data); // same data as uploading event
+});
+ftpDeploy.on("log", function(data) {
+  console.log(data); // same data as uploading event
+});
+ftpDeploy.on("upload-error", function(data) {
+  console.log(data.err); // data will also include filename, relativePath, and other goodies
+});
+
 ftpDeploy.deploy(config, function(err, res) {
   if (err) {
     console.log("Error deploying..");
